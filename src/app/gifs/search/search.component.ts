5f1eb15ba9ece289e6af5c27700cd7ad65d5fdef
Jonathan San Miguel <guerreroz80@outlook.es>
2022-11-08 13:26:26 -0600

    YamatoV3

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value
+    console.log(valor)
+    this.txtBuscar.nativeElement.value = ''
   }
-
 }
