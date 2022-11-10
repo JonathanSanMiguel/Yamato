56a9b22da9522316824b36b210a0363f55c6bf02
Jonathan San Miguel <guerreroz80@outlook.es>
2022-11-09 19:54:40 -0600

    YamatoV7

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,20 +15,30 @@ export class GifsService {
     return [...this._historial]
   }//historial
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) {
+    //Carga desde el LocalStorage el historial
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+    //Carga desde el LocalStorage la ultima busqueda
+    this.results = JSON.parse(localStorage.getItem('results')!) || []
+  }
 
   bucarGifs( query: string ){
+    //Al dato de entrada, le quita los espacios en blanco y lo pasa a minusculas
     query = query.trim().toLowerCase()
-
+    
     if( !this._historial.includes(query) ){
       this._historial.unshift(query)
       this._historial = this._historial.splice(0,10)
+      //Guarda el historial en el LocalStorage
+      localStorage.setItem('historial', JSON.stringify(this._historial))
     }//if
 
-    //el subscribe se ejecuta cuando tengamos una respuesta del get
+    //El subscribe se ejecuta cuando tengamos una respuesta del get
     this.http.get<SearchGifResponse>(`http://api.giphy.com/v1/gifs/search?api_key=psjMr58q6QG8CMdMnBnVE5isJp1EneYE&q=${query}`)
       .subscribe((resp) => {
         this.results = resp.data
+        //Guarda los resultados en el LocalStorage
+        localStorage.setItem('results', JSON.stringify(this.results))
       })
   }//buscarGifs
 
